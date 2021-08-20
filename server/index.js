de35b85e6dab1baf20b06b363c8219ecd2d93d53
jonahvimahi <jonahvimahi@gmail.com>
2021-08-20 09:30:32 -0600

    added fortunes button with functionality

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const app = express();
 
 
 app.use(cors());
-
 app.use(express.json()); // When we want to be able to accept JSON.
 
 app.get("/api/compliment", (req, res) => {
@@ -20,6 +19,21 @@ app.get("/api/compliment", (req, res) => {
 
   res.status(200).send(randomCompliment);
   
+});
+
+app.get("/api/fortune", (req, res) => {
+  const fortunes = [
+    "A dubious friend may be an enemy in camoflauge",
+    "It is better to deal with problems before they arise",
+    "It is honorable to stand up for what is right, however unpopular is seems",
+    "New ideas could be profitable",
+    "No on can walk backwards in the future"
+  ];
+
+  let randomIndex = Math.floor(Math.random() * fortunes.length);
+  let randomFortune = fortunes[randomIndex]
+
+  res.status(200).send(randomFortune);
 });
 
 app.listen(4000, () => console.log("Server running on 4000"));
