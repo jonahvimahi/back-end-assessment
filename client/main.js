2cb6a943d7fb69d48a1fc2a37c8e95b879532f0f
jonahvimahi <jonahvimahi@gmail.com>
2021-08-23 20:51:51 -0600

    Added  simple 3rd party api

--- client/main.js
+++ client/main.js
@@ -1,53 +1,76 @@
-document.getElementById("submit").onclick = function () {
-    axios.post("http://localhost:4000/api/person/")
-    .then(function (response) {
-      document.getElementById("person").value = data
-      console.log(phrase)
-      // console.log(data)
-      alert(data)
-      
-    })
-  }
+// none of the code below that is commented out is working
 
-  document.getElementById("complimentButton").onclick = function () {
-    axios.get("http://localhost:4000/api/compliment/")
-    .then(function (response) {
-      const data = response.data;
-      alert(data);
-    });
-  };
+// const firstName = document.querySelector("#firstName");
+// const lastName = document.querySelector("#lastName");
+// const hobby = document.querySelector("#hobby");
+// const goal = document.querySelector("#goal");
+const goals = document.getElementById('goals')
+const priority = document.getElementById('priority-dropdown')
+// document.getElementById("submit").onclick = function () {
+//   axios.post("http://localhost:4000/api/person/").then(function (response)
 
-  document.getElementById("fortuneButton").onclick = function () {
-    axios.get("http://localhost:4000/api/fortune/")
-    .then(function (response) {
-      const data = response.data;
-      alert(data);
-    });
-  };
+//     const phrase = `What's up ${firstName} ${lastName}?? You're the best at ${hobby} you will for sure ${goal}!!`;
 
-  document.getElementById("jarjar").onclick = function () {
-    axios.get("http://localhost:4000/api/jarjar/")
-    .then(function (response) {
-      const data = response.data;
-      alert(data);
-    });
-  };
+//     document.getElementById("person").value = data;
+//     console.log(phrase);
+//     // console.log(data)
+//     alert(data);
+//   });
+// };
 
-  document.getElementById("batman").onclick = function () {
-    axios.get("http://localhost:4000/api/batman/")
-    .then(function (response) {
-      const data = response.data;
-      alert(data);
-    });
-  };
+// get compliment button
+document.getElementById("complimentButton").onclick = function () {
+  axios.get("http://localhost:4000/api/compliment/").then(function (response) {
+    const data = response.data;
+    alert(data);
+  });
+};
+// get fortune button
+document.getElementById("fortuneButton").onclick = function () {
+  axios.get("http://localhost:4000/api/fortune/").then(function (response) {
+    const data = response.data;
+    alert(data);
+  });
+};
+// jar-jar binks quote
+document.getElementById("jarjar").onclick = function () {
+  axios.get("http://localhost:4000/api/jarjar/").then(function (response) {
+    const data = response.data;
+    alert(data);
+  });
+};
+// batman quote
+document.getElementById("batman").onclick = function () {
+  axios.get("http://localhost:4000/api/batman/").then(function (response) {
+    const data = response.data;
+    alert(data);
+  });
+};
+// donald trump quote
+document.getElementById("trump").onclick = function () {
+  axios.get("http://localhost:4000/api/trump/").then(function (response) {
+    const data = response.data;
+    alert(data);
+  });
+};
 
-  document.getElementById("trump").onclick = function () {
-    axios.get("http://localhost:4000/api/trump/")
-    .then(function (response) {
-      const data = response.data;
-      alert(data);
-    });
-  };
+document.getElementById('kanye').onclick = function () {
+  axios.get('https://api.kanye.rest').then(function (response) {
+    const data = response.data.quote
+    alert(data + ' -Kanye mf West')
+  })
+}
+// Was gonna try to use this but i never made a git ignore before i pushed so now im screwed
+
+// gifBtn.addEventListener("click", () => {
+//   while(gifSection.firstchild) {
+//     const gif = document.createElement("img");
+//     gif.setAttribute("src", res.data.data.image.original.url);
+//     gif.setAttribute("alt", "Hey");
+//     gif.setAttribute("id","gif");
+//     gifSection.appendChild(gif);
+//   }
+// })
 
 //   document.getElementById("beHappy").onclick = function () {
 //     axios.post("http://localhost:4000/api/meme/")
--- server/index.js
+++ server/index.js
@@ -3,14 +3,10 @@ const cors = require("cors");
 
 const app = express();
 
-
 app.use(cors());
 app.use(express.json()); // When we want to be able to accept JSON.
 
-
-
 app.get("/api/person/", (req, res) => {
-
   //      const newPerson = {
   //   firstName: firstName.value,
   //   lastName: lastName.value,
@@ -18,24 +14,22 @@ app.get("/api/person/", (req, res) => {
   //   goal: goal.value,
   // };
 
-
   // const firstName = document.querySelector("#firstName")
   // const lastName = document.querySelector("#lastName")
   // const hobby = document.querySelector("#hobby")
   // const goal = document.querySelector("#goal")
 
+  const { firstName, lastName, hobby, goal } = req.body;
+  const phrase = `What's up ${firstName} ${lastName}?? You're the best at ${hobby} you will for sure ${goal}!!`;
 
-  const {firstName, lastName, hobby, goal} = req.body;
-  const phrase = `What's up ${firstName} ${lastName}?? You're the best at ${hobby} you will for sure ${goal}!!`
-
-
-  res.status(200).send(phrase)
+  res.status(200).send(phrase);
 });
 
 app.get("/api/compliment/", (req, res) => {
-  const compliments = ["Gee, you're a smart cookie!",
-					 "Cool shirt!",
-					 "Your Javascript skills are stellar.",
+  const compliments = [
+    "Gee, you're a smart cookie!",
+    "Cool shirt!",
+    "Your Javascript skills are stellar.",
   ];
 
   // choose random compliment
@@ -43,7 +37,6 @@ app.get("/api/compliment/", (req, res) => {
   let randomCompliment = compliments[randomIndex];
 
   res.status(200).send(randomCompliment);
-  
 });
 
 app.get("/api/fortune", (req, res) => {
@@ -52,18 +45,18 @@ app.get("/api/fortune", (req, res) => {
     "It is better to deal with problems before they arise",
     "It is honorable to stand up for what is right, however unpopular is seems",
     "New ideas could be profitable",
-    "No on can walk backwards in the future"
+    "No on can walk backwards in the future",
   ];
 
   let randomIndex = Math.floor(Math.random() * fortunes.length);
-  let randomFortune = fortunes[randomIndex]
+  let randomFortune = fortunes[randomIndex];
 
   res.status(200).send(randomFortune);
 });
 
 app.get("/api/jarjar/", (req, res) => {
   const jarjarQuote = [
-   "Obi? Obi! Mesa So Smilen To Seein Yousa! - Jar-Jar Binks"
+    "Obi? Obi! Mesa So Smilen To Seein Yousa! - Jar-Jar Binks",
   ];
 
   res.status(200).send(jarjarQuote);
@@ -71,7 +64,7 @@ app.get("/api/jarjar/", (req, res) => {
 
 app.get("/api/batman/", (req, res) => {
   const batmanQuote = [
-   "It's not who I am underneath, but what I do that defines me. - Bruce Wayne"
+    "It's not who I am underneath, but what I do that defines me. - Bruce Wayne",
   ];
 
   res.status(200).send(batmanQuote);
@@ -79,17 +72,17 @@ app.get("/api/batman/", (req, res) => {
 
 app.get("/api/trump/", (req, res) => {
   const trumpQuote = [
-   "Nobody has better respect for intelligence than Donald Trump - Donald Trump"
+    "Nobody has better respect for intelligence than Donald Trump - Donald Trump",
   ];
 
   res.status(200).send(trumpQuote);
 });
 
 app.post("/api/meme/", (req, res) => {
-  const funny = "http://apimeme.com/meme?meme=You-The-Real-MVP-2&top=When+Jonah+finally+gets+an+api+to+work&bottom="
-  let photo = document.createElement(p)
-  element.appendChild(photo)
-  res.status(200).send(funny)
-  
-})
+  const funny =
+    "http://apimeme.com/meme?meme=You-The-Real-MVP-2&top=When+Jonah+finally+gets+an+api+to+work&bottom=";
+  let photo = document.createElement(p);
+  element.appendChild(photo);
+  res.status(200).send(funny);
+});
 app.listen(4000, () => console.log("Server running on 4000"));
